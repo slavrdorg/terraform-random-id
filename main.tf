0aadfd0f24bd9a27759c3394f568a3d152a73a6a
Slav Donchev <slav.donchev@gmail.com>
2020-08-19 14:57:47 +0300

    add provider constraints

--- main.tf
+++ main.tf
@@ -1,13 +1,20 @@
 resource "random_id" "id" {
-#  keepers = {
-#    uuid = uuid()
-#  }
+  #  keepers = {
+  #    uuid = uuid()
+  #  }
   byte_length = var.byte_length
 }
 
 data "null_data_source" "id" {
   inputs = {
     random_id = random_id.id.hex
-    uuid = uuid()
+    uuid      = uuid()
+  }
+}
+
+terraform {
+  required_providers {
+    random_id = "2.1.2"
+    null      = "2.2.0"
   }
 }
